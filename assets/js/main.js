2d5e810be2c4569aee97bd0fd0e9c6ea5fde6ec8
Bojan Janevski <bojan.janevski@iqvia.com>
2024-05-05 23:17:42 +0300

    chore: Update dropdown menu style and URL for subscription form

--- assets/js/main.js
+++ assets/js/main.js
@@ -574,7 +574,7 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
 
             $.ajax({
                 type: 'POST',
-                url: 'assets/php/subscribe.php',
+                url: 'php/subscribe.php',
                 dataType: 'json',
                 data: {
                     email: email
